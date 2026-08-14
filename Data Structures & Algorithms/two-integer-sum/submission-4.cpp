0345ec9f7702803fb9a635f1c03752b62556e37f
ANIKET SINGH <75739821+ANIKETSINGH09@users.noreply.github.com>
2026-08-14 18:44:14 +0530

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+
+        unordered_map<int,int> m;
+
+        for(int i=0;i<n;i++)
+        {
+            int ans= target-nums[i];
+
+            if(m.count(ans))
+            {
+                return {m[ans],i};
+            }
+
+            m[nums[i]]=i;
+        }
+    }
+};
